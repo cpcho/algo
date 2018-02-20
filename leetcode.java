fe8eaf3fd3c8f40f5803b689d81c91f5fa8a66e9
Cho <591159@bah.com>
2018-02-19 20:45:07 -0500

    commit

--- leetcode.java
+++ leetcode.java
@@ -77,6 +77,50 @@ private ListNode reverseListInt(ListNode head, ListNode newHead){
 	return reverseListInt(next, head);
 }
 
+#2. Add Two Numbers
+//https://leetcode.com/problems/add-two-numbers/description/
+public ListNode addTwoNumbers(ListNode l1, ListNode l2){
+	ListNode c1 = l1;
+	ListNode c2 = l2;
+	ListNode sentinel = new ListNode(0);
+	ListNode d = sentinel;
+	int sum = 0;
+	while (c1 != null || c2 != null){
+		sum /= 10;
+		if (c1 != null) {
+			sum += c1.val;
+			c1 = c1.next;
+		}
+		if (c2 != null) {
+			sum += c2.val;
+			c2 = c2.next;
+		}
+		d.next = new ListNode(sum % 10);
+		d = d.next;
+	}
+	if (sum / 10 == 1)
+		d.next = new ListNode(1);
+	return sentinel.next;
+}
+##OR###
+
+public ListNode addTwoNumbers(ListNode l1, ListNode l2){
+	ListNode prev = new ListNode(0);
+	ListNode head = prev;
+	int carry = 0;
+	while (l1 != null || l2 != null || carry != 0){
+		ListNode curr = new ListNode(0);
+		int sum = ((l2 == null) ? 0 : l2.val) + ((l1 == null ? 0 : l2.val) + carry;
+		curr.val = sum % 10;
+		carry = sum / 10;
+		prev.next = curr;
+		prev = prev.next;
+
+		l1 = (l1 == null) ? l1 : l1.next;
+		l2 = (l2 == null) ? l2 : l2.next;
+	}
+	return head.next;
+}
 
 #100. Same Tree
 //https://leetcode.com/problems/same-tree/description/
