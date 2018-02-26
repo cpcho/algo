b8df5b3dc37a1e3bfd5cefe003c986e96d15896e
Cho <591159@bah.com>
2018-02-25 22:16:25 -0500

    commit

--- Graph/F100.Same_Tree.java
+++ Graph/F100.Same_Tree.java
@@ -35,19 +35,19 @@ Input:     1         1
 Output: false*/
 
 public boolean isSameTree(TreeNode p, TreeNode q) {
-	// Equal nullity denotes that this branch is the same (local equality)
-  // This is a base case, but also handles being given two empty trees
+// Equal nullity denotes that this branch is the same (local equality)
+// This is a base case, but also handles being given two empty trees
 	if(p == null && q == null) return true;
 
-	// Unequal nullity denotes that the trees aren't the same
-  // Note that we've already ruled out equal nullity above
+// Unequal nullity denotes that the trees aren't the same
+// Note that we've already ruled out equal nullity above
 	if(p == null || q == null) return false;
 
-	// Both nodes have values; descend iff those values are equal
-  // "&&" here allows for any difference to overrule a local equality
+// Both nodes have values; descend iff those values are equal
+// "&&" here allows for any difference to overrule a local equality
 	if(p.val == q.val)
 		return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
 
-	// If we're here, both nodes have values, and they're unequal, so the trees aren't the same
+// If we're here, both nodes have values, and they're unequal, so the trees aren't the same
 	return false;
 }
