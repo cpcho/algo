c6b09a3ea9543987c19dc3dfe67337583a979d62
Cho <591159@bah.com>
2018-02-25 22:15:16 -0500

    commit

--- Graph/F100.Same_Tree.java
+++ Graph/F100.Same_Tree.java
@@ -1,6 +1,5 @@
 #100. Same Tree (Easy)
-/*
-Given two binary trees, write a function to check if 
+/*Given two binary trees, write a function to check if 
 they are the same or not.
 Two binary trees are considered the same if they are 
 structurally identical and the nodes have the same value.
@@ -33,20 +32,19 @@ Input:     1         1
 
         [1,2,1],   [1,1,2]
 
-Output: false
-*/
+Output: false*/
 
 public boolean isSameTree(TreeNode p, TreeNode q) {
 	// Equal nullity denotes that this branch is the same (local equality)
-    // This is a base case, but also handles being given two empty trees
+  // This is a base case, but also handles being given two empty trees
 	if(p == null && q == null) return true;
 
 	// Unequal nullity denotes that the trees aren't the same
-    // Note that we've already ruled out equal nullity above
+  // Note that we've already ruled out equal nullity above
 	if(p == null || q == null) return false;
 
 	// Both nodes have values; descend iff those values are equal
-    // "&&" here allows for any difference to overrule a local equality
+  // "&&" here allows for any difference to overrule a local equality
 	if(p.val == q.val)
 		return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
 
