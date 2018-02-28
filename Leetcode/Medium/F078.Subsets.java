baf72fcaa2b407ecb11650d67c97e41b3d97c3f1
Cho <591159@bah.com>
2018-02-28 00:02:26 -0500

    commit

---
+++ Leetcode/Medium/F078.Subsets.java
@@ -0,0 +1,35 @@
+#78. Subsets (medium)
+/*Given a set of distinct integers, nums, return all possible subsets (the power set).
+
+Note: The solution set must not contain duplicate subsets.
+
+For example,
+If nums = [1,2,3], a solution is:
+
+[
+  [3],
+  [1],
+  [2],
+  [1,2,3],
+  [1,3],
+  [2,3],
+  [1,2],
+  []
+]
+*/
+
+public List<List<Integer>> subsets(int[] nums) {
+    List<List<Integer>> list = new ArrayList<>();
+    Arrays.sort(nums);
+    backtrack(list, new ArrayList<>(), nums, 0);
+    return list;
+}
+
+private void backtrack(List<List<Integer>> list , List<Integer> tempList, int [] nums, int start){
+    list.add(new ArrayList<>(tempList));
+    for(int i = start; i < nums.length; i++){
+        tempList.add(nums[i]);
+        backtrack(list, tempList, nums, i + 1);
+        tempList.remove(tempList.size() - 1);
+    }
+}
---
+++ Leetcode/Medium/F215.Kth_Largest_Element_in_an_Array.java
@@ -0,0 +1,85 @@
+#215. Kth Largest Element in an Array (medium)
+
+/*Find the kth largest element in an unsorted array. 
+Note that it is the kth largest element in the sorted order, 
+not the kth distinct element.
+
+For example,
+Given [3,2,1,5,6,4] and k = 2, return 5.
+
+Note: 
+You may assume k is always valid, 1 ≤ k ≤ array's length.*/
+
+//Priority queue
+//O(N lg K) running time + O(K) memory
+public int findKthLargest(int[] nums, int k) {
+
+	final PriorityQueue<Integer> pq = new PriorityQueue<>();
+	for(int val : nums) {
+		pq.offer(val);
+
+		if(pq.size() > k) {
+			pq.poll();
+		}
+	}
+	return pq.peek();
+}
+
+#OR
+
+//Selection algorithm
+//O(N) best case / O(N^2) worst case running time + O(1) memory
+public int findKthLargest(int[] nums, int k) {
+
+	shuffle(nums); //O(N) guaranteed running time + O(1) space
+	k = nums.length - k;
+	int lo = 0;
+	int hi = nums.length - 1;
+	while (lo < hi) {
+		final int j = partition(nums, lo, hi);
+		if(j < k) {
+			lo = j + 1;
+		} else if  (j > k) {
+			hi = j - 1;
+		} else {
+			break;
+		}
+	}
+	return nums[k];
+}
+
+private int partition(int[] a, int lo, int hi) {
+
+	int i = lo;
+	int j = hi + 1;
+
+	while(true) {
+		while(i < hi && less(a[++i], a[lo]));
+		while(j > lo && less(a[lo], a[--j]));
+		if(i >= j) {
+			break;
+		}
+		exch(a, i ,j);
+	}
+	exch(a, lo, j);
+	return j;
+}
+
+private void exch(int[] a, int i, int j) {
+	final int tmp = a[i];
+	a[i] = a[j];
+	a[j] = tmp;
+}
+
+private boolean less(int v, int w) {
+	return v < w;
+}
+
+private void shuffle(int a[]) {
+
+	final Random random = new Random();
+	for (int i = i; i < a.length; i++) {
+		final int r = random.nextInt(i + 1);
+		exch(a, ind, r);
+	}
+}
