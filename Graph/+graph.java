a3198ca8cb06ef3d5b6b58b3118f48bcb63d9978
Cho <591159@bah.com>
2018-03-02 22:54:03 -0500

    commit

--- Graph/+graph.java
+++ Graph/+graph.java
@@ -1,5 +1,132 @@
 //https://stackoverflow.com/a/20024503
 
+class Node {
+	int value;
+	Node left, right;
+
+	public Node(int value) {
+		this.value = value;
+		left = right = null;
+	}
+}
+
+class BinaryTree {
+	Node root;
+
+	//we have to find the place where we want to add a new node in order to keep the tree sorted. 
+	private Node addRecursive(Node curr, int val) {
+		//when the current node is null, we’ve reached a leaf node and we can insert the new node in that position
+		if (curr == null) return new Node(val);
+
+		if (val < curr.value) {
+			//if the new node’s value is lower than the current node’s, we go to the left child
+			curr.left = addRecursive(curr.left, value);
+		} else if (val > curr.value) {
+			//if the new node’s value is greater than the current node’s, we go to the right child
+			curr.right = addRecursive(curr.right, value);
+		} else {
+			//value already exists
+			return curr;
+		}
+		return curr;
+	}
+	//Next, create the public method that starts the recursion from the root node.
+	public void add(int val) {
+		root = addRecursive(root, val);
+	}
+
+	private boolean containsNodeRecursive(Node curr, int val) {
+		if (curr == null) return false;
+		if (val == curr.value) return true;
+		return val < curr.value ? containsNodeRecursive(curr.left, val) : containsNodeRecursive(curr.right, val);
+	}
+
+	public boolean containsNode(int val) {
+		//create the public method that starts from the root.
+		return containsNodeRecursive(root, val);
+	}
+
+	/*Deletion*/
+	private Node deleteRecursive(Node curr, int val) {
+		if (curr == null) return null;
+		if (val == curr.value) {
+			// Case 1: no children
+			if (curr.left == null && curr.right == null) return null;
+			
+			// Case 2: only 1 child
+			if (curr.right == null) return curr.left;
+			if (curr.left == null) return curr.right;
+
+			// Case 3: 2 children
+			int smallestVal = findSmallestVal(curr.right);
+			curr.value = smallestVal;
+			curr.right = deleteRecursive(curr.right, smallestVal);
+			return curr;
+		}
+
+		if (val < curr.value) {
+			curr.left = deleteRecursive(curr.left, val);
+			return curr;
+		}
+
+		curr.right = deleteRecursive(curr.right, val);
+		return curr;
+	}
+
+	private int findSmallestVal(Node root) {
+		return root.left == null ? root.value : findSmallestVal(root.left);
+	}
+
+	public void delete(int val) {
+		deleteRecursive(root, val);
+	}
+
+	//In-order traversal
+	public void trasverseInOrder(Node node) {
+		if (node != null) {
+			trasverseInOrder(node.left);
+			System.out.print(" " + node.value);
+			trasverseInOrder(node.right);
+		}
+	}
+	//Pre-order traversal
+	public void traversePreOrder(Node node) {
+		if (node != null) {
+			System.out.print(" " + node.value);
+			traversePreOrder(node.left);
+			traversePreOrder(node.right);
+		}
+	} 
+
+	//Post-order traversal
+	public void traversePostOrder(Node node) {
+		if (node != null) {
+			traversePostOrder(node.left);
+			traversePostOrder(node.right);
+			System.out.print(" " + node.value);
+		}
+	}
+
+	//Breadth-First Search
+	public void traverseLevelOrder() {
+		if (root == null) return;
+
+		Queue<Node> nodes = new LinkedList<>();
+		nodes.add(root);
+
+		while (!nodes.isEmpty()) {
+			Node node = nodes.remove();
+			System.out.print(" " + node.value);
+
+			if (node.left != null) nodes.add(node.left);
+			if (node.right != null) nodes.add(node.right);
+		}
+	}
+
+
+}
+
+
 
 DFS
 /*
@@ -34,7 +161,7 @@ public class Graph {
 		Node d = getNode(dest);
 		s.adj.add(d);
 	}
-
+	//DFS Path
 	public boolean hasPathDFS(int src, int dest) {
 		Node s = getNode(src);
 		Node d = getNode(dest);
@@ -55,7 +182,7 @@ public class Graph {
 		return false;
 	}
 
-
+	//BFS Path
 	public boolean hasPathBFS(int src, int dest) {
 	//Helper method for BFS
 		return hasPathBFS(getNode(src), getNode(dest));
--- Leetcode/Easy/F543.Diameter_of_Binary_Tree.java
+++ Leetcode/Easy/F543.Diameter_of_Binary_Tree.java
@@ -20,26 +20,24 @@ it = MaxDepth of its left subtree + MaxDepth of its right subtree.
 Per the problem, the diameter of a binary tree is the length of 
 the longest path between any two nodes in a tree.*/
 
-int max = 0;
-    
-public int diameterOfBinaryTree(TreeNode root) {
-    maxDepth(root);
-    return max;
-}
+public Solution{
+//Time: O(N). Visiting every node once.
+//Space: O(N), the size of implicit call stack during our depth-first search.
+  int max = 0;
+      
+  public int diameterOfBinaryTree(TreeNode root) {
+      maxDepth(root);
+      return max;
+  }
 
-private int maxDepth(TreeNode root) {
-    if (root == null) return 0;
-    
-    int left = maxDepth(root.left);
-    int right = maxDepth(root.right);
-    
-    max = Math.max(max, left + right);
-    
-    return Math.max(left, right) + 1;
+  private int maxDepth(TreeNode root) {
+      if (root == null) return 0;
+      
+      int left = maxDepth(root.left);
+      int right = maxDepth(root.right);
+      
+      max = Math.max(max, left + right);
+      
+      return Math.max(left, right) + 1;
+  }
 }
-
-/*
-#Time: O(N). Visiting every node once.
-#Space: O(N), the size of implicit call stack 
- during our depth-first search.
-*/
