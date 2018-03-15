589b4566010df44e319795bd14b67907cf611a91
Cho <591159@bah.com>
2018-03-15 19:59:40 -0400

    modified names

--- Note/LinkedList/LinkedList.java
+++ Note/LinkedList/LinkedList.java
@@ -4,8 +4,7 @@
 //https://stackoverflow.com/a/9517015/5922564
 
 ###Pseudocode###
-LIST-SEARCH(L,k)
-// theta(n) in the worst case, since it may have to search the entire list
+LIST-SEARCH(L,k) // theta(n) in the worst case, since it may have to search the entire list.
 	x = L.head;
 	while (x != null && x.key != key) {
 		x = x.next;
@@ -13,12 +12,10 @@ LIST-SEARCH(L,k)
 	return x
 
 LIST-INSERT(L, x)
-// produces "splices" x onto the front of the linked list
-// O(1) running time
-
-// Recall that our attribute notation can cascade, so that
-// L.head.prev denotes the prev attribute of the objct that
-// L.head points to.
+/*Produces "splices" x onto the front of the linked list.
+O(1) Time.
+Recall that our attribute notation can cascade, so that L.head.prev denotes the 
+prev attribute of the objct that L.head points to.*/
 
 	x.next = L.head;
 	if (L.head != null)
@@ -27,13 +24,11 @@ LIST-INSERT(L, x)
 	x.prev = null;
 
 LIST-DELETE(L, x)
-// O(1) running time without giving a key.
-// If given key, however, worst case would be theta(n) b/c 
-// we must call LIST-SEARCH first to find the element.
-// It removes an element x from a linked list L.
-// It must be given a pointer to x, and it then "splices" x
-// out of the list by updating pointers.
-	if(x.prev != null) {
+/*O(1) running time without giving a key.
+If given key, however, worst case would be theta(n) b/c we must call LIST-SEARCH first to find the element.
+It removes an element x from a linked list L. It must be given a pointer to x, and it then "splices" x
+out of the list by updating pointers.*/
+	if (x.prev != null) {
 		x.prev.next = x.next;
 	} else {
 		L.head = x.next;
@@ -41,22 +36,165 @@ LIST-DELETE(L, x)
 	if (x.next != null)
 		x.next.prev = x.prev;
 
-###################################
-###Implementation of Linked List###
-###################################
+/*Linked List by Gayle*/
+public class Node {
+	Node next;
+	int data;
+	public Node(int data) {
+		this.data = data;
+	}
+}
+
+public class LinkedList {
+	Node head; // Wrap our head to distinguish new head from existing head
+
+	public void append(int data) {
+		if (head == null) head = new Node(data);
+		Node curr = head;
+		while (curr.next != null) {
+			curr = curr.next; // Keep moving and get to the end of the Linked List
+		}
+		curr.next = new Node(data); // Create a new node at the end of the Linked List
+	}
+
+	public void prepend(int data) {
+		Node newHead = new Node(data); // Create a newHead node
+		newHead.next = head; // newHead's next value will link to the old head
+		head = newHead; // We need to change the head pointer, making newHead to be head
+	}
+
+	public void deleteWithValue(int data) {
+		if (head == null) return;
+		if (head.data == data) {
+			head = head.next; // Delete the head if the data to be deleted is in head
+			return;
+		}
+		Node curr = head;
+		while (curr.next != null) { // Walks through the Linked List and get to the end of the list
+			if (curr.next.data == data) {
+				curr.next = curr.next.next;
+				return;
+			}
+			curr = curr.next;
+		}
+	}
+}
+
+/*Linked List by Joe James. https://www.youtube.com/watch?v=ch1uQeu0PVY*/
+private class Node {
+	private Node nextNode;
+	private int data;
+
+	private Node() {} // 0-arg constructor, 1-arg constructor, 2-arg constructor
+	
+	private Node(int val) {
+		data = val;
+	}
+	
+	private Node(int val, Node next) {
+		data = val;
+		nextNode = next;
+	}
+	
+	private void setData(int val) {
+		this.data = val;
+	}
+	
+	private int getData() {
+		return this.data;
+	}
+	
+	private void setNextNode(Node n) {
+		this.nextNode = n;
+	}
+	
+	private Node getNextNode() {
+		return this.nextNode;
+	}
+}
+
+public class LinkedList {
+	Node root;
+	int size;
+	
+	public LinkedList() {
+		root = new Node();
+		size = 0;
+	}
+	
+	// Test code - main function
+	public static void main(String[] args) {
+		LinkedList ll = new LinkedList();
+		System.out.println(ll.getSize());
+		ll.add(8);
+		System.out.println(ll.getSize());
+		ll.add(17);
+		ll.add(5);
+		ll.add(10);
+		System.out.println(ll.find(17).getData());
+		ll.remove(5);
+		System.out.println(ll.getSize());
+		System.out.println(ll.find(5));
+	}
+	
+	public void setSize(int s) {
+		this.size = s;
+	}
+	
+	public int getSize() {
+		return this.size;
+	}
+	
+	public Node add(int data) {
+		Node newNode = new Node(data, root);
+		this.root = newNode;
+		this.size++;
+		return newNode;
+	}
+	
+	public Node find(int data) {
+		Node thisNode = this.root;
+		
+		while (thisNode != null) {
+			if (thisNode.getData() == data)
+				return thisNode;
+			thisNode = thisNode.getNextNode();
+		}
+		return null;
+	}
+	
+	public boolean remove(int data) {
+		Node thisNode = this.root;
+		Node prevNode = null;
+		
+		while (thisNode != null) {
+			if (thisNode.getData() == data) {
+				if (prevNode != null)
+					prevNode.setNextNode(thisNode.getNextNode());
+				else
+					this.root = null;
+				this.setSize(this.getSize()-1);
+				return true;
+			}
+			prevNode = thisNode;
+			thisNode = thisNode.getNextNode();
+		}
+		return false;
+	}
+}
+
+/*Linked List Implementation*/
 class Link{
 	public int data1;
 	public double data2;
 	public Link nextLink;
 	
-	//Link constructor
-	public Link(int d1, double d2){
+	public Link(int d1, double d2) { //Link constructor
 		data1 = d1;
 		data2 = d2;
 	}
-	
-	//Print Link data
-	public void printLink(){
+
+	public void printLink() { //Print Link data
 		System.out.print("{" + data1 + ", " + data2 + "} ");
 	}
 }
@@ -64,40 +202,32 @@ class Link{
 class LinkList{
 	private Link head;
 	
-	//LinkList constructor
-	public LinkList(){
-		//Initialize the firstList is always null.
-		head = null;
+	public LinkList() { // LinkList constructor
+		head = null; // Initialize the firstList is always null.
 	}
 	
-	//Returns true if list is empty
-	public boolean isEmpty(){
-		return head == null;
+	public boolean isEmpty() {
+		return head == null; // Returns true if list is empty
 	}
 	
-	//Inserts a new Link at the first of the list
-	public void insert(int d1, double d2){
+	public void insert(int d1, double d2) {
 		Link link = new Link(d1, d2);
 		link.nextLink = head;
-		head = link;
+		head = link; // Inserts a new Link at the first of the list
 	}
 	
-	//Deletes the link at the first of the list
-	public Link delete(){
+	
+	public Link delete() { // Deletes the link at the first of the list
 		Link temp = head;
-		if(head == null){
-			return null;
-			//throw new NoSuchElementException(); //better way
-		}
+		if(head == null) throw new NoSuchElementException(); // Better way
 		head = head.nextLink;
 		return temp;
 	}
-	
-	//Prints list data
-	public void printList(){
+
+	public void printList(){ //	Prints list data
 		Link currentLink = head;
 		System.out.print("List: ");
-		while(currentLink != null){
+		while(currentLink != null) {
 			currentLink.printLink();
 			currentLink = currentLink.nextLink;
 		}
@@ -307,166 +437,3 @@ class NeighborIterator{
 		}
 	}
 }
-
-###Linked List  - Gayle Version###
-public class Node {
-	Node next;
-	int data;
-	public Node(int data){
-		this.data = data;
-	}
-}
-
-public class LinkedList { 
-//Will wrap our head in order to distinguish new head from existing head
-	Node head;
-
-	public void append(int data){
-		if (head == null) {
-			head = new Node(data);
-			return;
-		}
-		Node current = head;
-		while (current.next != null){
-			current = current.next; 
-			//Keep moving and get to the end of the Linked List
-		}
-		current.next = new Node(data); 
-		//Create a new node at the end of the Linked List
-	}
-
-	public void prepend(int data) {
-		Node newHead = new Node(data); //Create a newHead node
-		newHead.next = head; 
-		//newHead's next value will link to the old head
-		head = newHead; 
-		//We need to change the head pointer, making newHead to be head
-	}
-
-	public void deleteWithValue(int data) {
-		if (head == null) return;
-		if (head.data == data) {
-			head = head.next; 
-			//Delete the head if the data to be deleted is in head
-			return;
-		}
-			
-		Node current = head;
-		while (current.next != null) { 
-		//Walks through the Linked List and get to the end of the list
-			if (current.next.data == data) {
-				current.next = current.next.next;
-				return;
-			}
-			current = current.next;
-		}
-	}
-}
-
-###Linked List in Java. Written by Joe James.###
-//https://www.youtube.com/watch?v=ch1uQeu0PVY
-// Node class
-private class Node {
-	private Node nextNode;
-	private int data;
-
-	// 0-arg constructor, 1-arg constructor, 2-arg constructor
-	private Node() { }
-	
-	private Node(int val) {
-		data = val;
-	}
-	
-	private Node(int val, Node next) {
-		data = val;
-		nextNode = next;
-	}
-	
-	private void setData(int val) {
-		this.data = val;
-	}
-	
-	private int getData() {
-		return this.data;
-	}
-	
-	private void setNextNode(Node n) {
-		this.nextNode = n;
-	}
-	
-	private Node getNextNode() {
-		return this.nextNode;
-	}
-}
-
-public class LinkedList {
-	Node root;
-	int size;
-	
-	public LinkedList() {
-		root = new Node();
-		size = 0;
-	}
-	
-	// Test code - main function
-	public static void main(String[] args) {
-		LinkedList ll = new LinkedList();
-		System.out.println(ll.getSize());
-		ll.add(8);
-		System.out.println(ll.getSize());
-		ll.add(17);
-		ll.add(5);
-		ll.add(10);
-		System.out.println(ll.find(17).getData());
-		ll.remove(5);
-		System.out.println(ll.getSize());
-		System.out.println(ll.find(5));
-	}
-	
-	public void setSize(int s) {
-		this.size = s;
-	}
-	
-	public int getSize() {
-		return this.size;
-	}
-	
-	public Node add(int data) {
-		Node newNode = new Node(data, root);
-		this.root = newNode;
-		this.size++;
-		return newNode;
-	}
-	
-	public Node find(int data) {
-		Node thisNode = this.root;
-		
-		while (thisNode != null) {
-			if (thisNode.getData() == data)
-				return thisNode;
-			thisNode = thisNode.getNextNode();
-		}
-		return null;
-	}
-	
-	public boolean remove(int data) {
-		Node thisNode = this.root;
-		Node prevNode = null;
-		
-		while (thisNode != null) {
-			if (thisNode.getData() == data) {
-				if (prevNode != null)
-					prevNode.setNextNode(thisNode.getNextNode());
-				else
-					this.root = null;
-				this.setSize(this.getSize()-1);
-				return true;
-			}
-			prevNode = thisNode;
-			thisNode = thisNode.getNextNode();
-		}
-		return false;
-	}
-}
-
-
